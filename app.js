22e0c1dfa4f715fd63aa724a524644886ef79080
irayya <ihiremath1991@gmail.com>
2021-09-24 23:30:23 +0530

    function added

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ let image=document.querySelector('.image')
 let imageArray=['images/mt1.jpg','images/mt2.jpeg','images/mt6.jpg','images/mt7.jpeg','images/mt9.jpeg','images/mt5.jpg']
 let imageIndex=1;
 
-// function changeImage(){
-//     image.setAttribute("src",imageArray[imageIndex]);
-//     imageIndex ++;
-// }
 
-// alert(imgArray)
 
 function next(){
 // image.style.backgroundImage=`url({imageArray[imageIndex]})`
@@ -30,6 +25,12 @@ function back(){
 
 
 }
+// function changeImage(){
+//     image.setAttribute("src",imageArray[imageIndex]);
+//     imageIndex ++;
+// }
+
+// alert(imgArray)
 
 //   function changeImage(){
 //       imageIndex ++;
