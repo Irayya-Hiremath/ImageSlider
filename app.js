87b45d6ec1fbeaba8e0d1bf7da9479e98729c207
irayya <ihiremath1991@gmail.com>
2021-10-05 22:55:38 +0530

    images changed

--- app.js
+++ app.js
@@ -1,46 +1,32 @@
-        
-        
-let image=document.querySelector('.image')
-        
-let imageArray=['images/mt1.jpg','images/mt2.jpeg','images/mt6.jpg','images/mt7.jpeg','images/mt9.jpeg','images/mt5.jpg']
-let imageIndex=1;
+let image = document.querySelector(".image");
 
+let imageArray = [
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpeg",
+  "images/5.jpg",
+  "images/6.jpg",
+  "images/7.jpg",
+  "images/8.jpg",
+];
+let imageIndex = 1;
 
-
-function next(){
-// image.style.backgroundImage=`url({imageArray[imageIndex]})`
-    image.style.backgroundImage = `url(${imageArray[imageIndex]})`;
-        if(imageIndex==5){
-           imageIndex=0
-        }
-        else{
-        imageIndex++;
-
-        }   
+function next() {
+  // image.style.backgroundImage=`url({imageArray[imageIndex]})`
+  image.style.backgroundImage = `url(${imageArray[imageIndex]})`;
+  if (imageIndex == 5) {
+    imageIndex = 0;
+  } else {
+    imageIndex++;
   }
-
-function back(){
- image.style.backgroundImage = `url(${imageArray[imageIndex]})`;
- if(imageIndex==1){
-   imageIndex=5
-    }
-    else{
-      imageIndex--;
-
-    }
-
- 
-
-
 }
-// function changeImage(){
-//     image.setAttribute("src",imageArray[imageIndex]);
-//     imageIndex ++;
-// }
 
-// alert(imgArray)
-
-//   function changeImage(){
-//       imageIndex ++;
-//   }
-
+function back() {
+  image.style.backgroundImage = `url(${imageArray[imageIndex]})`;
+  if (imageIndex == 1) {
+    imageIndex = 5;
+  } else {
+    imageIndex--;
+  }
+}
